e00a5205e1537240814ec74782236ead8c5a5294
yossef5725 <a0548475725@gmail.com>
2024-03-13 17:04:34 +0200

    fix message

--- src/server/yemot-api.ts
+++ src/server/yemot-api.ts
@@ -12,7 +12,7 @@ YemotRouts.get('/details', api.withRemult, async (req, res)=> {
     if(!people) {
         res.send("id_list_message=t-מספר טלפון לא קיים במערכת.&go_to_folder=/")
     } else {
-        const message = `id_list_message=t-להלן הפרטים כפי שמעודכנים במערכת: שם: ${people.firstName} ${people.lastName}: כתובת: ${people.address}: בנין: ${people.building} קומה: ${people.floor}: דירה: ${people.apartment}: מספר ילדים: ${people.children}: מספר עופות: ${people.poultry}: כשרות: ${people.cosher}: אלו הפרטים שמעודכנים אצלינו, במידה וחלק מהפרטים לא נכונים אנא עדכן אותנו בהקדם.&go_to_folder=/`
+        const message = `id_list_message=t-להלן הפרטים כפי שמעודכנים במערכת: שם: ${people.firstName} ${people.lastName}: כתובת: ${people.address}: בנין: ${people.building} קומה: ${people.floor.replaceAll('-', ' מינוס ')}: דירה: ${people.apartment}: מספר ילדים: ${people.children}: מספר עופות: ${people.poultry}: כשרות: ${people.cosher}: אלו הפרטים שמעודכנים אצלינו במידה וחלק מהפרטים לא נכונים אנא עדכן אותנו בהקדם.&go_to_folder=/`
         res.send(message)
     }
 
